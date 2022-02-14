34552f39128c3800020104fdac17451b58218bdf
Jethro Williams <jethrowilliams@outlook.com>
2022-02-14 14:53:57 +0000

    Add other blocks

--- src/store/game-board.js
+++ src/store/game-board.js
@@ -14,6 +14,70 @@ const blockObject = (status, color) => {
   };
 };
 
+const newBlockShape = block => {
+  switch (block) {
+    case 'I':
+      return newBlockI();
+    case 'J':
+      return newBlockJ();
+    case 'L':
+      return newBlockL();
+    case 'O':
+      return newBlockO();
+    case 'S':
+      return newBlockS();
+    case 'T':
+      return newBlockT();
+    case 'Z':
+      return newBlockZ();
+    default:
+      return newBlockO();
+  }
+};
+
+const newBlockI = () => {
+  const blockI = blockObject('live', 'red');
+
+  return {
+    1: {
+      4: blockI,
+      5: blockI,
+      6: blockI,
+      7: blockI,
+    },
+  };
+};
+
+const newBlockJ = () => {
+  const blockJ = blockObject('live', 'gold');
+
+  return {
+    1: {
+      4: blockJ,
+    },
+    2: {
+      4: blockJ,
+      5: blockJ,
+      6: blockJ,
+    },
+  };
+};
+
+const newBlockL = () => {
+  const blockL = blockObject('live', 'blue');
+
+  return {
+    1: {
+      6: blockL,
+    },
+    2: {
+      4: blockL,
+      5: blockL,
+      6: blockL,
+    },
+  };
+};
+
 const newBlockO = () => {
   const blockO = blockObject('live', 'green');
 
@@ -25,6 +89,51 @@ const newBlockO = () => {
     2: {
       5: blockO,
       6: blockO,
+    },
+  };
+};
+
+const newBlockS = () => {
+  const blockS = blockObject('live', 'chocolate');
+
+  return {
+    1: {
+      5: blockS,
+      6: blockS,
+    },
+    2: {
+      4: blockS,
+      5: blockS,
+    },
+  };
+};
+
+const newBlockT = () => {
+  const blockT = blockObject('live', 'orange');
+
+  return {
+    1: {
+      5: blockT,
+    },
+    2: {
+      4: blockT,
+      5: blockT,
+      6: blockT,
+    },
+  };
+};
+
+const newBlockZ = () => {
+  const blockZ = blockObject('live', 'fuchsia');
+
+  return {
+    1: {
+      4: blockZ,
+      5: blockZ,
+    },
+    2: {
+      5: blockZ,
+      6: blockZ,
     },
   };
 };
@@ -56,10 +165,12 @@ const gameBoardSlice = createSlice({
   },
   reducers: {
     nextBlock(state) {
-      state.liveBlock = blocks[Math.floor(Math.random() * blocks.length)];
+      let newBlock = blocks[Math.floor(Math.random() * blocks.length)];
+
+      state.liveBlock = newBlock;
       if ((state.blockCounter + 1) % 10 === 0) state.speed = state.speed * 0.75;
       state.blockCounter = state.blockCounter + 1;
-      state.squares = mergeNestedObjects(current(state.squares), newBlockO());
+      state.squares = mergeNestedObjects(current(state.squares), newBlockShape(newBlock));
       state.timerIsLive = true;
     },
     updateGameBoard(state, action) {
