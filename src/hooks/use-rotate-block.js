b48418520f318948704c9694f9fabfb4e7240d3a
Jethro Williams <jethrowilliams@outlook.com>
2022-02-16 12:08:53 +0000

    Calculate the most populated column

--- src/hooks/use-rotate-block.js
+++ src/hooks/use-rotate-block.js
@@ -19,27 +19,73 @@ const useRotateBlock = () => {
     return returnObject;
   };
 
+  const uniqueRows = shape => {
+    return Object.keys(shape).map(rowKey => parseInt(rowKey));
+  };
+
+  const allColumns = shape => {
+    let columnsArray = [];
+    Object.keys(shape).forEach(outerKey => columnsArray.push(Object.keys(shape[outerKey])));
+
+    return columnsArray.flat().map(column => parseInt(column));
+  };
+
+  const uniqueColumns = shape => {
+    return [...new Set(allColumns(shape))];
+  };
+
+  const columnAmountsObject = shape => {
+    let columnAmounts = {};
+
+    uniqueColumns(shape).forEach(
+      column => (columnAmounts[column] = allColumns(shape).filter(x => x === column).length)
+    );
+
+    return columnAmounts;
+  };
+
+  const mostPopulatedColumn = shape => {
+    return Object.keys(columnAmountsObject(shape)).find(
+      columnKey =>
+        columnAmountsObject(shape)[columnKey] ===
+        Math.max(...Object.values(columnAmountsObject(shape)))
+    );
+  };
+
   const numberOfRows = shape => {
-    return Object.keys(shape).length;
+    return uniqueRows(shape).length;
   };
 
   const numberOfColumns = shape => {
-    let columnsArray = [];
-    Object.keys(shape).forEach(outerKey => columnsArray.push(Object.keys(shape[outerKey])));
-    let uniqueColumns = [...new Set(columnsArray.flat().map(columnKey => parseInt(columnKey)))];
-
-    return Math.max(...uniqueColumns) - Math.min(...uniqueColumns) + 1;
+    return Math.max(...uniqueColumns(shape)) - Math.min(...uniqueColumns(shape)) + 1;
   };
 
   const newShape = startingShape => {
-    console.log(numberOfColumns(startingShape));
-
     let returnObject = {};
 
+    // console.log(uniqueRows(startingShape));
+    // console.log(numberOfRows(startingShape));
+    // console.log(uniqueColumns(startingShape));
+    // console.log(numberOfColumns(startingShape));
+
     if (numberOfRows(startingShape) >= 3) {
-      // TODO
+      // TODO - Unfinished
+      let pivotRow = uniqueRows(startingShape)[1];
+
+      numberOfColumns(startingShape).forEach(
+        (_, index) => (returnObject[pivotRow + (index - 1)] = {})
+      );
+
+      console.log(returnObject);
     } else if (numberOfColumns(startingShape) >= 3) {
-      // TODO
+      // let pivot = uniqueRows(startingShape)[1];
+
+      // numberOfColumns(startingShape).forEach(
+      //   (_, index) => (returnObject[pivot + (index - 1)] = {})
+      // );
+
+      // console.log(returnObject);
+      console.log(mostPopulatedColumn(startingShape));
     }
 
     // TODO
