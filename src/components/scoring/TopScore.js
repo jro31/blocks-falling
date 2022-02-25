d0640dcadc7fc9967bcb3bb1a29d805e0385bfd6
Jethro Williams <jethrowilliams@outlook.com>
2022-02-25 12:46:34 +0000

    Store top score in local storage

--- src/components/scoring/TopScore.js
+++ src/components/scoring/TopScore.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { gameOver } from '../../store/game-board';
 import Scoreboard from './Scoreboard';
 
 const TopScore = () => {
-  return <Scoreboard name='Top score' score={999} />;
+  const status = useSelector(state => state.gameBoard.status);
+  const clearedRows = useSelector(state => state.gameBoard.clearedRows);
+  const [topScore, setTopScore] = useState(localStorage.getItem('tetris.top-score'));
+
+  useEffect(() => {
+    if (status === gameOver) {
+      if ((topScore && clearedRows > topScore) || !topScore) {
+        setTopScore(clearedRows);
+        localStorage.setItem('tetris.top-score', clearedRows);
+      }
+    }
+  }, [status]);
+
+  return <Scoreboard name='Top score' score={topScore || 0} />;
 };
 
 export default TopScore;
